cd468a854c4e98f6d9f2dad3827848ae24f6e75b
WizeInge <thomas.burgio@ynov.com>
2016-12-08 15:40:22 +0100

    Add loader, update index & css

--- renderer.js
+++ renderer.js
@@ -1,22 +1,29 @@
-//Quit App Btn
-const remote = require('electron').remote;
 window.$ = window.jQuery = require('jquery')
-
-const closeAppBtn = document.getElementById('closeApp')
-closeAppBtn.addEventListener('click', function(){
-  console.log("closeApp")
-  var window = remote.getCurrentWindow();
-  window.close();
-})
 
 //Hover img
 $.each($('.wallpaper'), function(){
-  $(this).on('mouseenter', function(e){
-    console.log(e);
-    $(this).css('width', '250')
+  $(this).on('mouseenter', function(){
+    $(this).css('width', '320')
   })
-  $(this).on('mouseleave', function(e){
-    console.log(e);
+  $(this).on('mouseleave', function(){
     $(this).css('width', '120')
   })
 })
+//OnClick img
+$.each($('.wallpaper'), function(){
+  $(this).on('click', function(){
+    $('#displayFs').fadeToggle();
+    $('#flou').fadeToggle();
+    $('#displayFs').css('background', 'url(' + $(this).find('img').attr('src') + ') center center');
+  })
+})
+
+$('#displayFs').on('click', function(){
+  $('#displayFs').fadeToggle();
+  $('#flou').fadeToggle();
+})
+
+//Loader
+setTimeout(function(){
+    $('#loader').fadeToggle()
+}, 8000)
